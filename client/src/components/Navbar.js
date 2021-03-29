857982621d1b5f088a54e47ffaf231188496643f
7rvn <valentinhobusch@icloud.com>
2021-03-29 19:04:18 +0200

    populated navbar with some links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,7 +11,41 @@ function Navbar() {
           history.push("/");
         }}
       >
-        MemoChess
+        Chess
+      </div>
+      <div className="nav-subtitle">PGN with video</div>
+      <div
+        className="nav-link"
+        onClick={() => {
+          history.push("/gothamchess");
+        }}
+      >
+        GothamChess
+      </div>
+      <div
+        className="nav-link"
+        onClick={() => {
+          history.push("/white");
+        }}
+      >
+        White openings
+      </div>
+      <div
+        className="nav-link"
+        onClick={() => {
+          history.push("/black");
+        }}
+      >
+        Black openings
+      </div>
+      <div className="nav-subtitle">PGN only</div>
+      <div
+        className="nav-link"
+        onClick={() => {
+          history.push("/custom");
+        }}
+      >
+        import PGN
       </div>
     </div>
   );
