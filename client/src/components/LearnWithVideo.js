d6f7cfdf96f0c1e3aa16b36449b1bb28a09e338e
7rvn <valentinhobusch@icloud.com>
2021-03-29 16:40:53 +0200

    move validation and enemy movement

--- client/src/components/LearnWithVideo.js
+++ client/src/components/LearnWithVideo.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 import Chess from "chess.js";
 
-// import { playSound } from "../utils/utils";
+import { playSound } from "../chessboard/src/utils/utils";
 import { constructPgnTree, getGoodMoves } from "../utils/pgnHelper";
 import { algToHex, hexToSan } from "../utils/helper";
 
@@ -31,44 +31,59 @@ function LearnWithVideo() {
   /* ************ */
   const boardRef = React.useRef();
 
-  // function handleMove(hexmove) {
-  //   const move = game.move({
-  //     from: hexToSan(hexmove.from.rank, hexmove.from.file),
-  //     to: hexToSan(hexmove.to.rank, hexmove.to.file),
-  //   });
+  function handleMove(hexmove) {
+    const move = game.move({
+      from: hexToSan(hexmove.from.rank, hexmove.from.file),
+      to: hexToSan(hexmove.to.rank, hexmove.to.file),
+    });
 
-  //   boardRef.current.setBoard(game.board());
-  //   move.flags = move.san.includes("+") ? move.flags + "+" : move.flags;
-  //   playSound(move.flags);
-  //   if (move) {
-  //     const goodMoves = getGoodMoves(currentNode);
-  //     const node = goodMoves.find((e) => e.move === move.san);
-  //     const moveRecommended = node ? true : false;
+    boardRef.current.setBoard(game.board());
 
-  //     if (moveRecommended) {
-  //       setGame(game);
-  //       setCurrentNode(node);
+    if (move) {
+      move.flags = move.san.includes("+") ? move.flags + "+" : move.flags;
+      playSound(move.flags);
 
-  //       boardRef.current.addHighlights({
-  //         squares: [hexmove.from, hexmove.to],
-  //         type: "lastMove",
-  //       });
-  //     } else {
-  //       boardRef.current.addHighlights({
-  //         squares: [hexmove.from, hexmove.to],
-  //         type: "wrongMove",
-  //       });
-  //       setTimeout(() => {
-  //         game.undo();
-  //         boardRef.current.setBoard(game.board());
-  //         boardRef.current.addHighlights({
-  //           squares: [],
-  //           type: "wrongMove",
-  //         });
-  //       }, 777);
-  //     }
-  //   }
-  // }
+      if (currentNode.nextMove) {
+        const goodMoves = getGoodMoves(currentNode);
+        const node = goodMoves.find((e) => e.move === move.san);
+        const moveRecommended = node ? true : false;
+
+        if (moveRecommended) {
+          setGame(game);
+          setCurrentNode(node);
+
+          boardRef.current.addHighlights({
+            squares: [hexmove.from, hexmove.to],
+            type: "lastMove",
+          });
+        } else {
+          boardRef.current.addHighlights({
+            squares: [hexmove.from, hexmove.to],
+            type: "wrongMove",
+          });
+          setTimeout(() => {
+            game.undo();
+            boardRef.current.setBoard(game.board());
+            boardRef.current.addHighlights({
+              squares: [],
+              type: "wrongMove",
+            });
+          }, 777);
+        }
+      } else {
+        boardRef.current.addHighlights({
+          squares: [hexmove.from, hexmove.to],
+          type: "lastMove",
+        });
+      }
+    } else {
+      const sanTo = hexToSan(hexmove.to.rank, hexmove.to.file);
+      // if clicked own piece
+      if (game.turn() === game.get(sanTo)?.color) {
+        return "samecolor";
+      }
+    }
+  }
 
   function handleActivatingPiece({ rank, file }) {
     const sanFrom = hexToSan(rank, file);
@@ -121,39 +136,40 @@ function LearnWithVideo() {
     const newGame = new Chess();
     setGame(newGame);
     boardRef.current.setBoard(newGame.board());
+    boardRef.current.addHighlights({ squares: [], type: "lastMove" });
   }
 
   function togglePgn() {
     setPgnVisible(pgnVisible ? false : true);
   }
 
-  // React.useEffect(() => {
-  //   if (currentNode.nextMove === null) {
-  //     return;
-  //   }
-  //   if (game.turn() !== opening.color[0]) {
-  //     const goodMoves = getGoodMoves(currentNode);
-  //     const node = goodMoves[Math.floor(Math.random() * goodMoves.length)];
+  React.useEffect(() => {
+    if (currentNode.nextMove === null) {
+      return;
+    }
+    if (game.turn() !== opening.color[0]) {
+      const goodMoves = getGoodMoves(currentNode);
+      const node = goodMoves[Math.floor(Math.random() * goodMoves.length)];
 
-  //     setTimeout(() => {
-  //       const move = game.move(node.move);
-  //       move.flags = move.san.includes("+") ? move.flags + "+" : move.flags;
-  //       playSound(move.flags);
-  //       setCurrentNode(node);
-  //       setGame(game);
-  //       boardRef.current.setBoard(game.board());
-  //       const hexFrom = algToHex(move.from);
-  //       const hexTo = algToHex(move.to);
-  //       boardRef.current.addHighlights({
-  //         squares: [
-  //           { rank: hexFrom[0], file: hexFrom[1] },
-  //           { rank: hexTo[0], file: hexTo[1] },
-  //         ],
-  //         type: "lastMove",
-  //       });
-  //     }, 777);
-  //   }
-  // }, [game, opening, currentNode]);
+      setTimeout(() => {
+        const move = game.move(node.move);
+        move.flags = move.san.includes("+") ? move.flags + "+" : move.flags;
+        playSound(move.flags);
+        setCurrentNode(node);
+        setGame(game);
+        boardRef.current.setBoard(game.board());
+        const hexFrom = algToHex(move.from);
+        const hexTo = algToHex(move.to);
+        boardRef.current.addHighlights({
+          squares: [
+            { rank: hexFrom[0], file: hexFrom[1] },
+            { rank: hexTo[0], file: hexTo[1] },
+          ],
+          type: "lastMove",
+        });
+      }, 777);
+    }
+  }, [game, opening, currentNode]);
 
   return (
     <div style={{ display: "flex", minWidth: "100vw" }}>
@@ -162,7 +178,7 @@ function LearnWithVideo() {
         <div id="appgame">
           <Board
             ref={boardRef}
-            // onMakeMove={handleMove}
+            onMakeMove={handleMove}
             onActivatePiece={handleActivatingPiece}
             initialOrientation={opening.color}
           ></Board>
